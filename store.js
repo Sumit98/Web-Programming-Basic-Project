b2d6f54f968257fd71946dbbe0e6475365ad0548
Sumit Jha <rdxvvvvv@gmail.com>
2019-03-17 11:44:22 +0530

    Final I think

--- store.js
+++ store.js
@@ -70,7 +70,16 @@ function addItemToCart(title, price, imageSrc) {
             return
         }
     }
-  
+    var cartRowContents = `
+        <div class="cart-item cart-column">
+            <img class="cart-item-image" src="${imageSrc}" width="100" height="100">
+            <span class="cart-item-title">${title}</span>
+        </div>
+        <span class="cart-price cart-column">${price}</span>
+        <div class="cart-quantity cart-column">
+            <input class="cart-quantity-input" type="number" value="1">
+            <button class="btn btn-danger" type="button">REMOVE</button>
+        </div>`
     cartRow.innerHTML = cartRowContents
     cartItems.append(cartRow)
     cartRow.getElementsByClassName('btn-danger')[0].addEventListener('click', removeCartItem)
